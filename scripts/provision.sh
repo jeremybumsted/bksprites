01ffcce0361ecece002a7d91c6d57e92abadfa71
Jeremy Bumsted <jeremy@buildkite.com>
2026-02-28 14:56:42 -0700

    Add a basic build pipeline

---
+++ scripts/provision.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script will execute when scaling out your sprites
+# The controller will use this script as the provision script
+# by default, but you're free to provide your own provision
+# script to best suit your needs
